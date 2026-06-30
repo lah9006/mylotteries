63a95f59a5c153bf45ff9192870a01df8fe6401c
Lazaro Anaya Hidalgo <l.anayah9012@gmail.com>
2026-06-30 15:06:13 -0400

    Reescritos settings.py y build.sh con codificacion ASCII y creacion de superusuario

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Crear superusuario autom?ticamente si no existe
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')" | python manage.py shell
