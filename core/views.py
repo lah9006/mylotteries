9c8cb2190e5f10eea937c0023e1f2db96c0d434e
Lazaro Anaya Hidalgo <l.anayah9012@gmail.com>
2026-06-30 15:18:26 -0400

    Añadida interfaz pública con listado de sorteos y autenticación

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Sorteo
 
-# Create your views here.
+def home(request):
+    sorteos = Sorteo.objects.filter(activo=True).order_by('-fecha_sorteo')
+    return render(request, 'core/home.html', {'sorteos': sorteos})
+
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+class RegistroUsuario(CreateView):
+    form_class = UserCreationForm
+    template_name = 'core/registro.html'
+    success_url = reverse_lazy('login')
